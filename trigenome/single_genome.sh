20253521fb40db23341fd35c0fb8ba3abdb7ffeb
Abhinav Sharma <abhi18av@gmail.com>
2019-11-08 07:26:18 +0530

    filename refactor

--- trigenome/single_genome.sh
+++ trigenome/single_genome.sh
@@ -2,9 +2,9 @@
 
 rm single_genome_report.html
 rm single_genome_timeline.html
-rm flowchart_dag.html
+rm single_genome_flowchart_dag.html
 rm .nextflow.*
 
-nextflow single_genome.nf -with-report single_genome_report.html -with-timeline single_genome_timeline.html -with-dag flowchart_dag.html # -with-trace
+nextflow single_genome.nf -with-report single_genome_report.html -with-timeline single_genome_timeline.html -with-dag single_genome_flowchart_dag.html # -with-trace
 
-git add -f ./single_genome.sh ./single_genome.nf ./single_genome_report.html  ./single_genome_timeline.html  ./flowchart_dag.html  ./.nextflow.log
+git add -f ./single_genome.sh ./single_genome.nf ./single_genome_report.html  ./single_genome_timeline.html  ./single_genome_flowchart_dag.html  ./.nextflow.log
